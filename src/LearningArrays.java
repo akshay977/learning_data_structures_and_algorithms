10f7e59d2438fd2662f4cd4f9d369c155fe85a92
Akshay Chandrakanth <akshay.chandrakanth@vedak.com>
2024-10-06 19:27:42 +0530

    find second largest integer

--- src/LearningArrays.java
+++ src/LearningArrays.java
@@ -7,14 +7,18 @@ public class LearningArrays {
                 1. Search (unsorted or sorted array)
                 2. Insert
                 3. Delete
+                4. Get ith element
+                5. Update ith element
+                6. Insert at the end and delete from the end
+                    can be done in O(1) time
         */
         int arr[] = {5, 3, 8, 9, 10, 1, 99};
-        int ans = search(arr, 7, 1);
-        System.out.println("search index: " + ans);
+        // int ans = search(arr, 7, 1);
+        // System.out.println("search index: " + ans);
 
-        int arr2[] = {5, 3, 8, 9, 10, 1, 99, 0, 0};
-        int ans2 = insert(arr2, 7, 10, 9, 2);
-        System.out.println(Arrays.toString(arr2));
+        // int arr2[] = {5, 3, 8, 9, 10, 1, 99, 0, 0};
+        // int ans2 = insert(arr2, 7, 10, 9, 2);
+        // System.out.println(Arrays.toString(arr2));
 
         /*
             Time complexity of insert in dynamically sized arrays:
@@ -24,6 +28,14 @@ public class LearningArrays {
             When the dynamically sized array is full, it creates a new array
             double the size of original array and inserts the element.
         */
+        // int ans3 = deleteElement(arr, 7, 8);
+        // System.out.println("delete ans: " + ans3 + " " + Arrays.toString(arr));
+
+        int ans4 = findLargestElement(arr);
+        System.out.println("findLargestElement: " + ans4);
+
+        int ans5 = findSecondLargest(arr);
+        System.out.println("findSecondLargestElement: " + ans5);
     }
 
     /*
@@ -60,5 +72,64 @@ public class LearningArrays {
         arr[idx] = x;
 
         return n+1;
+    }
+
+    // Average time complexity is Theta(n)
+    public static int deleteElement(int[] arr, int n, int x) {
+        int i;
+        for (i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                break;
+            }
+        }
+
+        if (i == n) {
+            return n;
+        }
+
+        for (int j = i; j < n-1; j++) {
+            arr[j] = arr[j+1];
+        }
+
+        return n-1;
+    }
+
+    // Efficient Approach
+    public static int findLargestElement(int[] arr) {
+        int res = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[res]) {
+                res = i;
+            }
+        }
+
+        return res;
+    }
+
+    // Efficient Approach
+    public static int findSecondLargest(int[] arr, int n) {
+        int res = -1, largest = 0;
+
+        /*
+            Test cases:
+                1. arr[i] > arr[largest]
+                2. arr[i] == arr[largest]
+                3. arr[i] < arr[largest]
+                    a. res = -1 || arr[i] > arr[res]
+                    b. arr[i] < arr[res]
+        */
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[largest]) {
+                res = largest;
+                largest = i;
+            } else if (arr[i] < arr[largest]) {
+                if (res == -1 || arr[i] > arr[res]) {
+                    res = i;
+                }
+            }
+        }
+
+        return res;
     }
 }
